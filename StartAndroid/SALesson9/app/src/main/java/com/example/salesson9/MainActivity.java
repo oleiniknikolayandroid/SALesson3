0fb7e7aeca64838d7840f2def4d1a03c0997321c
DeaDm0r0zZz <oleiniknikolayandroid@gmail.com>
2019-09-10 21:59:46 +0300

    Lesson9

--- StartAndroid/SALesson9/app/src/main/java/com/example/salesson9/MainActivity.java
+++ StartAndroid/SALesson9/app/src/main/java/com/example/salesson9/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.salesson9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // найдем View-элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // создаем обработчик нажатия
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+        btnCancel.setOnClickListener(oclBtnCancel);
+        btnOk.setOnClickListener(oclBtnOk);
+        
     }
 }
