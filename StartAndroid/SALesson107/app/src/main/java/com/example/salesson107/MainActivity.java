7b2e3c8df0de3af0c76d1e7c168566a0484aa81a
DeaDm0r0zZz <oleiniknikolayandroid@gmail.com>
2019-09-12 21:30:17 +0300

    Lesson107

---
+++ StartAndroid/SALesson107/app/src/main/java/com/example/salesson107/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.salesson107;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
